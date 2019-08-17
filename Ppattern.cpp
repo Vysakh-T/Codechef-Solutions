dbed3921fc9c4a4e40e8cb0fc1dc2e99888b2522
Vysakh T <vysakh1801@gmail.com>
2019-08-17 17:48:58 +0530

    Solution to a certain pattern printing Problem

---
+++ Ppattern.cpp
@@ -0,0 +1,28 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+	int l,T;
+	cin>>T;
+	for(l=0;l<T;l++)
+	{
+	  int N,n=1,i,j,a[100][100];
+	  cin>>N;
+	  for(i=0;i<N;i++)
+	  {
+	      for(j=0;j<N;j++)
+	      {
+ 
+	      }
+	  }
+	 for(i=0;i<N;i++)
+	  {
+	      for(j=0;j<N;j++)
+	      {
+	         cout<<a[i][j]<<" ";
+	      }
+	      cout<<"\n";
+	  } 
+	}
+	return 0;
+}
